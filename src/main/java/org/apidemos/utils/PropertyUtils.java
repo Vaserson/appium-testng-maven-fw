e3ba47e7b290aff2f5812e5b14b1d8615efc3fa7
Vaserson <kadabra777@gmail.com>
2024-11-11 22:54:07 +0200

    added PropertyUtils.java

---
+++ src/main/java/org/apidemos/utils/PropertyUtils.java
@@ -0,0 +1,100 @@
+package org.apidemos.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
+
+public final class PropertyUtils {
+    private static final Logger logger = LogManager.getLogger(PropertyUtils.class);
+
+    private static Properties property = new Properties();
+    private static final Map<String, String> CONFIGMAP = new HashMap<>();
+
+
+    private PropertyUtils(){}
+
+    static {
+        try {
+            FileInputStream file = new FileInputStream("src/test/resources/apiDemos.properties");
+            property.load(file);
+            for(Map.Entry<Object, Object> entry : property.entrySet()) {
+                CONFIGMAP.put(String.valueOf(entry.getKey()), String.valueOf(entry.getValue()));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static HashMap<String, String> parseStringXML(InputStream file) throws Exception {
+        HashMap<String, String> stringMap = new HashMap<>();
+
+        // Get Document Builder
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        // Build Document
+        Document document = builder.parse(file);
+
+        // Normalize the XML Structure. TOO IMPORTANT!
+        document.getDocumentElement().normalize();
+
+/*        // Root node
+        Element root = document.getDocumentElement(); // resources from xml
+//        System.out.println(root.getNodeName());*/
+
+        // Get all elements
+        NodeList nList = document.getElementsByTagName("string");
+        System.out.println("=====================================");
+
+        for (int i = 0; i < nList.getLength(); i++) {
+            Node node = nList.item(i);
+            System.out.println(); // separator
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) node;
+                // Store each element key value in map
+                stringMap.put(eElement.getAttribute("name"), eElement.getTextContent());
+            }
+        }
+        return stringMap;
+    }
+
+    public static String getProperty(String key) {
+        logger.info("Searching system property '{}'", key);
+        String property = System.getProperty(key);
+        if (Objects.isNull(property)) {
+            logger.info("No system property for '{}' is found.", key);
+            return getConfigProperty(key);
+        } else {
+            logger.info("System property for '{}' is: {}].", key, property);
+            return property;
+        }
+    }
+
+    public static String getConfigProperty(String key) {
+        logger.info("Searching property '{}' inside diyConfig.properties file", key);
+        String property = CONFIGMAP.get(key);
+        if (Objects.nonNull(property)) {
+            logger.info("Property for '{}' is: {}.", key, property);
+            return property;
+        } else {
+            throw new RuntimeException("No '" + key + "' property found. Please check the documentation for instructions to setting up testing data");
+        }
+    }
+
+}
--- src/test/java/org/apidemos/BaseTest.java
+++ src/test/java/org/apidemos/BaseTest.java
@@ -4,38 +4,52 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apidemos.utils.PropertyUtils;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
 import java.time.Duration;
-import java.util.Properties;
+import java.util.HashMap;
 
 public class BaseTest {
     protected AndroidDriver driver;
     protected WebDriverWait wait;
-    protected Properties properties;
-    InputStream inputStream;
+    InputStream stringsXml;
+    protected static HashMap<String, String> strings = new HashMap<>();
 
     private static final Logger LOGGER = LogManager.getLogger(BaseTest.class);
+
+    @BeforeTest
+    public void beforeTest() throws IOException {
+        try {
+            String xmlFileName = "strings.xml"; //TODO Move to constants
+            stringsXml = getClass().getClassLoader().getResourceAsStream(xmlFileName);
+            strings = PropertyUtils.parseStringXML(stringsXml);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (stringsXml != null) {
+                stringsXml.close();
+            }
+        }
+    }
 
     @BeforeMethod
     public void setUp() {
         try {
-            properties = new Properties();
-            String propertiesFileName = "apiDemos.properties";
-            inputStream = getClass().getClassLoader().getResourceAsStream(propertiesFileName);
-            properties.load(inputStream);
 
-            URL url = new URI(properties.getProperty("appiumURL")).toURL();
+            URL url = new URI(PropertyUtils.getProperty("appiumURL")).toURL();
 
             UiAutomator2Options options = new UiAutomator2Options()
                     .setPlatformName("Android")
-                    .setAppPackage(properties.getProperty("androidAppPackage"))
-                    .setAppActivity(properties.getProperty("androidAppActivity"))
+                    .setAppPackage(PropertyUtils.getProperty("androidAppPackage"))
+                    .setAppActivity(PropertyUtils.getProperty("androidAppActivity"))
                     .noReset();
 
             driver = new AndroidDriver(url, options);
