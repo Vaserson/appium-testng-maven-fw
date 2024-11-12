471cf2d3848bde117adea54bd98353cf3a5117d9
Vaserson <kadabra777@gmail.com>
2024-11-12 10:05:21 +0200

    moved to DriverFactory

---
+++ src/main/java/org/apidemos/driver/DriverFactory.java
@@ -0,0 +1,30 @@
+package org.apidemos.driver;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.apidemos.utils.PropertyUtils;
+
+import java.net.URI;
+import java.net.URL;
+
+public class DriverFactory {
+    private static AppiumDriver driver;
+
+    public static AppiumDriver getDriver() {
+        try {
+            URL url = new URI(PropertyUtils.getProperty("appiumURL")).toURL();
+
+            UiAutomator2Options options = new UiAutomator2Options()
+                    .setPlatformName("Android")
+                    .setAppPackage(PropertyUtils.getProperty("androidAppPackage"))
+                    .setAppActivity(PropertyUtils.getProperty("androidAppActivity"))
+                    .noReset();
+
+            driver = new AndroidDriver(url, options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return driver;
+    }
+}
--- src/test/java/org/apidemos/BaseTest.java
+++ src/test/java/org/apidemos/BaseTest.java
@@ -1,12 +1,10 @@
 package org.apidemos;
 
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apidemos.driver.DriverFactory;
 import org.apidemos.utils.PropertyUtils;
 import org.apidemos.utils.TestUtils;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,14 +12,9 @@ import org.testng.annotations.BeforeTest;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
-import java.net.URL;
-import java.time.Duration;
 import java.util.HashMap;
 
 public class BaseTest {
-    protected AndroidDriver driver;
-    protected WebDriverWait wait;
     InputStream stringsXml;
     protected static HashMap<String, String> strings = new HashMap<>();
 
@@ -43,28 +36,12 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    public void setUp() {
-        try {
-
-            URL url = new URI(PropertyUtils.getProperty("appiumURL")).toURL();
-
-            UiAutomator2Options options = new UiAutomator2Options()
-                    .setPlatformName("Android")
-                    .setAppPackage(PropertyUtils.getProperty("androidAppPackage"))
-                    .setAppActivity(PropertyUtils.getProperty("androidAppActivity"))
-                    .noReset();
-
-            driver = new AndroidDriver(url, options);
-            wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    public void setUp() {}
 
     @AfterMethod
     public void tearDown(ITestResult result) {
         try {
-            driver.quit();
+            DriverFactory.getDriver().quit();
         } catch (Exception ignored) {}
 
         TestUtils.getScreenshotOnFailedMethod(result.getStatus(), result.getName());
